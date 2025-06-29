165be4b6019291b21e05943e3fb01b538b7345f5
BhupeshB7 <bhupeshkr2912@gmail.com>
2025-06-29 16:19:54 +0530

    update

--- validators/user.zod.js
+++ validators/user.zod.js
@@ -1,9 +1,9 @@
 import { z } from "zod";
 
 const userSchema = z.object({
-  name: z.string().min(3).max(50).optional(),
-  email: z.email(),
-  password: z.string().min(4).max(50),
+    name: z.string().min(3).max(50).optional(),
+    email: z.string().email(),  
+    password: z.string().min(4).max(50),
 });
 
 export default userSchema;
